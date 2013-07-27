65416a8484770e84027812ad1e89a51a176d76d3
pcrane <trigunshin@gmail.com>
2013-07-27 00:54:36 -0400

    fixed some derps...

--- puppet-modules.sh
+++ puppet-modules.sh
@@ -1,12 +1,14 @@
 #!/bin/sh -e
+cd ~
+wget "http://apt.puppetlabs.com/puppetlabs-release-quantal.deb"
+dpkg -i "puppetlabs-release-quantal.deb"
+sudo apt-get update -y
+sudo apt-get install -y puppet-common
+rm "http://apt.puppetlabs.com/puppetlabs-release-quantal.deb"
+
 if ! puppet module list | grep -q puppetlabs-nodejs; then
     puppet module install puppetlabs-nodejs
 fi
 if ! puppet module list | grep -q stdlib; then
     puppet module install puppetlabs/stdlib
 fi
-cd ~
-wget "http://apt.puppetlabs.com/puppetlabs-release-quantal.deb"
-dpkg -i "puppetlabs-release-quantal.deb"
-sudo apt-get update -y
-sudo apt-get install puppet-common
