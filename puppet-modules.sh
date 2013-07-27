d1b3424a90dbb1948fb581a17ff4817f8f64e78b
pcrane <trigunshin@gmail.com>
2013-07-27 01:05:09 -0400

    deal with this minor cleanup later...

--- puppet-modules.sh
+++ puppet-modules.sh
@@ -4,7 +4,6 @@ wget "http://apt.puppetlabs.com/puppetlabs-release-quantal.deb"
 dpkg -i "puppetlabs-release-quantal.deb"
 sudo apt-get update -y
 sudo apt-get install -y puppet-common
-rm "http://apt.puppetlabs.com/puppetlabs-release-quantal.deb"
 
 if ! puppet module list | grep -q puppetlabs-nodejs; then
     puppet module install puppetlabs-nodejs
